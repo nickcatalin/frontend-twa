ad36dd7e48a7344713c2a5b9b10f3400594f0257
nickcatalin <florin.nicola@stud.acs.upb.ro>
2024-04-08 17:10:09 +0300

    first comit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import './index.css'; // Adjust the path to where your CSS file is located
+import logo from './logo.svg'
+import { Button } from 'primereact/button';
+import 'primereact/resources/themes/saga-green/theme.css';  //theme
+import 'primereact/resources/primereact.min.css';          //core css
+import 'primeicons/primeicons.css';                        //icons
+import AuthContent from './AuthContent';
 
 function App() {
   return (
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <AuthContent />
+
     </div>
+
   );
 }
 
